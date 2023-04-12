cafc36c3bf611c5b7ed02c5dffd4357ab40d9a7c
Nikita Sychev <root@nsychev.ru>
2023-04-12 21:40:53 +0600

    Fix interface name length

--- quirck/box/relay/client-connect.sh
+++ quirck/box/relay/client-connect.sh
@@ -2,7 +2,7 @@
 
 source /run/network.env
 ip=${trusted_ip:-$trusted_ip6}
-interface=mv-${trusted_ip//[\.:]/}-$(printf '%04x' $trusted_port)
+interface=mv-$(echo "${trusted_ip:-$trusted_ip6}:$trusted_port" | md5sum | head -c 12)
 network=${UV_NETWORK:-$NETWORK_DEFAULT}
 var_name="NETWORK_HWADDR_$network"
 
--- quirck/box/relay/learn-address.sh
+++ quirck/box/relay/learn-address.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ip=${trusted_ip:-$trusted_ip6}
-interface=mv-${trusted_ip//[\.:]/}-$(printf '%04x' $trusted_port)
+interface=mv-$(echo "${trusted_ip:-$trusted_ip6}:$trusted_port" | md5sum | head -c 12)
 
 if [[ "$1" == "delete" ]]; then
     ip link set link dev $interface type macvlan macaddr del ${2%%@*}
