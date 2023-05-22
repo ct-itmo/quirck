f27c0fba258bbfe631f37e1415eca7dfdc3b3f52
Nikita Sychev <root@nsychev.ru>
2023-05-22 22:45:52 +0600

    Make DockerConflict 409 instead of 500

--- quirck/box/exception.py
+++ quirck/box/exception.py
@@ -1,4 +1,9 @@
-class DockerConflict(Exception): ...
+from starlette.exceptions import HTTPException
+
+
+class DockerConflict(HTTPException):
+    def __init__(self):
+        super().__init__(409, f"Сейчас происходит операция с другим заданием: запустить проверку или задание невозможно")
 
 
 __all__ = ["DockerConflict"]
